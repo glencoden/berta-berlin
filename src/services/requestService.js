ffd9360b93c75ec5c0fdf30cd26c839267249d6b
glen coden <simon.der.meyer@gmail.com>
2022-05-16 12:52:34 +0200

    add deploy preview workflow

--- src/services/requestService.js
+++ src/services/requestService.js
@@ -1,11 +1,18 @@
-// cache
+/**
+ * Local cache
+ */
 import videoCache from '../cache/videos.json';
 import channelCache from '../cache/channel.json'
 
-// env
+/**
+ * Environment
+ */
+const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3';
+const CACHE_WEBWORKER_URL = 'https://berta.glencoden.workers.dev/';
 
 class RequestService {
+    // TODO implement URL API
     _get(url) {
         return fetch(url)
             .then(resp => resp.json())
@@ -15,17 +22,13 @@ class RequestService {
     getVideos() {
         return process.env.NODE_ENV === 'development'
             ? Promise.resolve(videoCache)
-            : this._get('https://berta.glencoden.workers.dev/'); // TODO update webworker url
+            : this._get(CACHE_WEBWORKER_URL);
     }
 
     getChannel() {
         return Promise.resolve(channelCache);
         // return this._get(`${YOUTUBE_API_URL}/channels?id=${YOUTUBE_CHANNEL_ID}&key=${YOUTUBE_API_KEY}&part=snippet,contentDetails,brandingSettings`);
     }
-
-    // getPlaylists() {
-    //     return this._get(`${YOUTUBE_API_URL}/playlists?channelId=${YOUTUBE_CHANNEL_ID}&key=${YOUTUBE_API_KEY}&part=contentDetails,player,snippet,status`);
-    // }
 }
 
 export const requestService = new RequestService();
